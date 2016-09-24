237ee27c521ad97e60fafb237a6efe1168f23030
Alexander Tumin <iamtakingiteasy@eientei.org>
2016-09-24 13:49:34 +0300

    Rearrange

---
+++ core/src/main/java/org/eientei/videostreamer/Application.java
@@ -0,0 +1,14 @@
+package org.eientei.videostreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Alexander Tumin on 2016-09-24
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

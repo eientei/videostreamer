cc2c6cb289f2d649a52ec62b22779b7ac5888148
Alexander Tumin <iamtakingiteasy@eientei.org>
2018-04-22 19:41:03 +0300

    or

--- server/context.go
+++ server/context.go
@@ -738,7 +738,7 @@ func (stream *Stream) AddSegment(newsamples []*mp4.Sample, sampledata []byte, ty
 		stream.VideoBuffer = append(stream.VideoBuffer, &Segment{Samples: newsamples, Data: sampledata, SliceType: slicetyp})
 	}
 
-	if vsegment.Len() > 0 && asegment.Len() > 0 {
+	if vsegment.Len() > 0 || asegment.Len() > 0 {
 		stream.SendSegment(vsegment.Bytes(), vsidx, vsamp, asegment.Bytes(), asidx, asamp)
 	}
 	return nil

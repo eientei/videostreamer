1e5a30391a71ff3d97f97ce5ddd6f0afffffe077
Alexander Tumin <iamtakingiteasy@eientei.org>
2018-04-25 15:00:59 +0300

    fsadf

--- mp4/muxer.go
+++ mp4/muxer.go
@@ -2,7 +2,6 @@ package mp4
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 )
 
@@ -474,7 +473,7 @@ func (muxer *Muxer) AddSampleData(sample *Sample, data []byte, typ uint8, slicet
 			h.MuxHandle(event)
 		}
 
-		fmt.Println(vidx, aidx)
+		//fmt.Println(vidx, aidx)
 		muxer.VideoBuffer = muxer.VideoBuffer[vidx:]
 		muxer.AudioBuffer = muxer.AudioBuffer[aidx:]
 	}

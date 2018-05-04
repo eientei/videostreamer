bca327d37c4f69cd3835d3a28d0e1794c3ae413c
Alexander Tumin <iamtakingiteasy@eientei.org>
2018-05-04 13:25:12 +0300

    dd

--- static/src/StreamList.js
+++ static/src/StreamList.js
@@ -19,11 +19,11 @@ class StreamList extends Component {
                                 <Button className={classes.maxwidth} onClick={() => dispatch(actions.navigation.play(stream.owner, stream.name))}>Watch</Button>
                             </div>
                             <div>
-                                <div>
+                                <div className={classes.flexrow}>
                                     <SvgIcon>
                                         <path fill="#000000" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
                                     </SvgIcon>
-                                    <Typography variant='headline'>Currently watching: {stream.clients}</Typography>
+                                    <Typography variant='headline'>{stream.clients}</Typography>
                                 </div>
                                 {
                                     user.notifications.find(n => n === stream.owner + (stream.name.length === 0 ? '' : ('/' + stream.name))) ?

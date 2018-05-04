4c5849bcf82d716fff61ebea363ccbaa19ba233f
Alexander Tumin <iamtakingiteasy@eientei.org>
2018-05-04 15:02:51 +0300

    dd

--- static/src/StreamList.js
+++ static/src/StreamList.js
@@ -20,10 +20,10 @@ class StreamList extends Component {
                             </div>
                             <div style={{width: '150px'}}>
                                 <div className={classes.flexrownopaddall}>
+                                    <Typography variant='headline'>{stream.clients}</Typography>
                                     <SvgIcon>
                                         <path fill="#000000" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
                                     </SvgIcon>
-                                    <Typography variant='headline'>{stream.clients}</Typography>
                                 </div>
                                 {
                                     user.notifications.find(n => n === stream.owner + (stream.name.length === 0 ? '' : ('/' + stream.name))) ?
